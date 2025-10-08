0861fb42c4bdc8429d9a5ca541acc5e923f589e4
Juliabergstron <julia.bergstron@etec.sp.gov.br>
2025-10-08 09:20:16 -0300

    Add files via upload

---
+++ MultiplosDeQuatro.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.multiplosdequatro;
+
+/**
+ *
+ * @author user
+ */
+
+
+public class MultiplosDeQuatro {
+
+    public static void main(String[] args) {
+    
+    for (int valor = 0; valor <= 30; valor++){
+    
+    if(valor % 4 != 0) {
+        
+    System.out.println(valor); 
+    }
+    
+    
+    }
+    
+    }
+}
